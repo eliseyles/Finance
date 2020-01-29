5713cfafe3026bb97b58829a6887eca4d76593ca
Alesya Khoroneko <horoneko00@gmail.com>
2020-01-29 14:08:16 +0300

    try to rm target

---
+++ src/test/by/training/finance/service/impl/TransactionLogImplTest.java
@@ -0,0 +1,32 @@
+package by.training.finance.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionLogImplTest {
+
+    @Test
+    public void addTransaction() {
+    }
+
+    @Test
+    public void updateTransaction() {
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void getAllTransactions() {
+    }
+
+    @Test
+    public void getCurrentBalance() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+}
